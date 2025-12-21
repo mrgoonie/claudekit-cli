e742d448471461bc99b0406bfe383831a4588c71
kaitranntt <kaitran.ntt@gmail.com>
2025-12-21 18:04:21 -0500

    refactor(help): extract individual command help definitions
    - create commands/ directory for help content
    
    - extract new, init, doctor, uninstall command help
    
    - separate update-command-help and versions-command-help
    
    - add common-options.ts for shared option definitions
    
    - reduce help-commands.ts from 479 to 60 lines (facade)

---
+++ src/domains/help/commands/index.ts
@@ -0,0 +1,17 @@
+/**
+ * Help Commands Index
+ *
+ * Re-exports all command help definitions.
+ */
+
+export { newCommandHelp } from "./new-command-help.js";
+export { initCommandHelp } from "./init-command-help.js";
+export { doctorCommandHelp } from "./doctor-command-help.js";
+export { uninstallCommandHelp } from "./uninstall-command-help.js";
+export { updateCommandHelp } from "./update-command-help.js";
+export { versionsCommandHelp } from "./versions-command-help.js";
+export {
+	projectOptionsGroup,
+	filterOptionsGroup,
+	folderOptionsGroup,
+} from "./common-options.js";
